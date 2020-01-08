321525fa06ab38c4bc62945dfb8165ab22b2a0fa
JimTheMan <mrdotjim@gmail.com>
2020-01-07 20:56:17 -0500

    initial jim vue app quasar scaffold

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Jims-Vue-Quasar-Test
-Example of a Vue.js project created with the Quasar Framework
+Example of a [Vue.js](https://vuejs.org/) project created with the [Quasar Framework](https://quasar.dev/).
+
+This project was created with the [quasar-cli](https://quasar.dev/quasar-cli):
+```
+npm i -g @quasar/cli
+```
+
+Then this command was run:
+```
+quasar create jims-vue-app
+```
+And these add-ons were chosen:
+◉ ESLint
+◉ Vuex
+◉ Axios
+
+Then just use the usual commands in `pacakge.json` to run the code!
+
+Start dev server:
+```
+npm start
+```
+Then visit: http://localhost:8080
+
+Run unit tests (with jest):
+```
+npm test
+```
+
+Build:
+```
+npm run build
+```
+
+---
+
+
+<img src="screenshot-menu-closed.png">
+<br/>
+<br/>
+
+<img src="screenshot-menu-open.png">
---
+++ jims-vue-app/src/layouts/MyLayout.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>Jims View App</q-toolbar-title>
+
+        <div>v0.0.1</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-2">
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://github.com/JimLynchCodes">
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Jim's Github Page</q-item-label>
+            <q-item-label caption>github.com/JimLynchCodes</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://linkedin.com/in/JimLynchCodes">
+          <q-item-section avatar>
+            <q-icon name="record_voice_over" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Jim's LinkedIn Page</q-item-label>
+            <q-item-label caption>linkedin.com/in/JimLynchCodes</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://twitter.com/JimLynchCodes">
+          <q-item-section avatar>
+            <q-icon name="rss_feed" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Jim's Twitter</q-item-label>
+            <q-item-label caption>twitter.com/JimLynchCodes</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://stackoverflow.com/users/1910355/jim"
+        >
+          <q-item-section avatar>
+            <q-icon name="public" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Jim's Stack Overflow</q-item-label>
+            <q-item-label caption>stackoverflow.com/users/1910355/jim</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://evaluates2.com/">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Evaluates2 Website</q-item-label>
+            <q-item-label caption>evaluates2.com/</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MyLayout",
+
+  data() {
+    return {
+      leftDrawerOpen: false
+    };
+  }
+};
+</script>
